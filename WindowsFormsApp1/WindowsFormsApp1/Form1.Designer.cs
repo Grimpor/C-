0bb58b6422e05776b9fd6da51528fd46d820fc3d
Grimpor <139313605+Grimpor@users.noreply.github.com>
2023-11-25 23:03:13 +0700

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Show = new System.Windows.Forms.Button();
+            this.X = new System.Windows.Forms.TextBox();
+            this.Y = new System.Windows.Forms.TextBox();
+            this.Calculate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sum = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.OK = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Show
+            // 
+            this.Show.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Show.Location = new System.Drawing.Point(553, 352);
+            this.Show.Name = "Show";
+            this.Show.Size = new System.Drawing.Size(142, 56);
+            this.Show.TabIndex = 0;
+            this.Show.Text = "Show";
+            this.Show.UseVisualStyleBackColor = true;
+            this.Show.Click += new System.EventHandler(this.Show_Click);
+            // 
+            // X
+            // 
+            this.X.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.X.Location = new System.Drawing.Point(270, 134);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(216, 32);
+            this.X.TabIndex = 1;
+            // 
+            // Y
+            // 
+            this.Y.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Y.Location = new System.Drawing.Point(270, 199);
+            this.Y.Name = "Y";
+            this.Y.Size = new System.Drawing.Size(216, 32);
+            this.Y.TabIndex = 2;
+            // 
+            // Calculate
+            // 
+            this.Calculate.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calculate.Location = new System.Drawing.Point(271, 264);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(216, 32);
+            this.Calculate.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(249, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 64);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Calculator";
+            // 
+            // sum
+            // 
+            this.sum.Font = new System.Drawing.Font("Microsoft YaHei", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sum.Location = new System.Drawing.Point(536, 142);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(63, 57);
+            this.sum.TabIndex = 5;
+            this.sum.Text = "+";
+            this.sum.UseVisualStyleBackColor = true;
+            this.sum.Click += new System.EventHandler(this.sum_Click);
+            // 
+            // minus
+            // 
+            this.minus.Font = new System.Drawing.Font("Microsoft YaHei", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minus.Location = new System.Drawing.Point(649, 142);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(63, 57);
+            this.minus.TabIndex = 6;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // multiply
+            // 
+            this.multiply.Font = new System.Drawing.Font("Microsoft YaHei", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiply.Location = new System.Drawing.Point(536, 234);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(63, 57);
+            this.multiply.TabIndex = 7;
+            this.multiply.Text = "*";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.multiply_Click);
+            // 
+            // divide
+            // 
+            this.divide.Font = new System.Drawing.Font("Microsoft YaHei", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divide.Location = new System.Drawing.Point(649, 234);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(63, 57);
+            this.divide.TabIndex = 8;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = true;
+            this.divide.Click += new System.EventHandler(this.divide_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(141, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 19);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "1st number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(141, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 19);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "2nd number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(141, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 19);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Result";
+            // 
+            // OK
+            // 
+            this.OK.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OK.Location = new System.Drawing.Point(94, 352);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(142, 56);
+            this.OK.TabIndex = 12;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clear.Location = new System.Drawing.Point(327, 352);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(142, 56);
+            this.Clear.TabIndex = 13;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.Y);
+            this.Controls.Add(this.X);
+            this.Controls.Add(this.Show);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Show;
+        private System.Windows.Forms.TextBox X;
+        private System.Windows.Forms.TextBox Y;
+        private System.Windows.Forms.TextBox Calculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button sum;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        float result = 0, x = 0, y = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void sum_Click(object sender, EventArgs e)
+        {
+            minus.BackColor = DefaultBackColor;
+            multiply.BackColor = DefaultBackColor;
+            divide.BackColor = DefaultBackColor;
+            sum.BackColor = Color.Gray;
+        }
+
+        public void minus_Click(object sender, EventArgs e)
+        {
+            sum.BackColor = DefaultBackColor;
+            multiply.BackColor = DefaultBackColor;
+            divide.BackColor = DefaultBackColor;
+            minus.BackColor = Color.Gray;
+        }
+
+        public void multiply_Click(object sender, EventArgs e)
+        {
+            sum.BackColor = DefaultBackColor;
+            minus.BackColor = DefaultBackColor;
+            divide.BackColor = DefaultBackColor;
+            multiply.BackColor = Color.Gray;
+
+        }
+
+        public void divide_Click(object sender, EventArgs e)
+        {
+            sum.BackColor = DefaultBackColor;
+            minus.BackColor = DefaultBackColor;
+            multiply.BackColor = DefaultBackColor;
+            divide.BackColor = Color.Gray;
+        }
+
+        private void Show_Click(object sender, EventArgs e)
+        {
+            x = Convert.ToInt32(X.Text);
+            y = Convert.ToInt32(Y.Text);
+            if (divide.BackColor == Color.Gray)
+            {
+                result = x / y;
+                Calculate.Text = result.ToString();
+                MessageBox.Show("You Choose Divide!!" + "\n" + x + " / " + y + " = " + result, "Result", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            }
+            else if(multiply.BackColor == Color.Gray)
+            {
+                result = x * y;
+                Calculate.Text = result.ToString();
+                MessageBox.Show("You Choose Multiply!!" + "\n" + x + " * " + y + " = " + result, "Result", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            }
+            else if(minus.BackColor == Color.Gray)
+            {
+                result = x - y;
+                Calculate.Text = result.ToString();
+                MessageBox.Show("You Choose Minus!!" + "\n" + x + " - " + y + " = " + result, "Result", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            }
+            else if(sum.BackColor == Color.Gray)
+            {
+                result = x + y;
+                Calculate.Text = result.ToString();
+                MessageBox.Show("You Choose Sum!!" + "\n" + x + " + " + y + " = " + result, "Result", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Please choose some method", "Result", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            x = Convert.ToInt32(X.Text);
+            y = Convert.ToInt32(Y.Text);
+            if (divide.BackColor == Color.Gray)
+            {
+                result = x / y;
+                Calculate.Text = result.ToString();
+            }
+            else if (multiply.BackColor == Color.Gray)
+            {
+                result = x * y;
+                Calculate.Text = result.ToString();
+            }
+            else if (minus.BackColor == Color.Gray)
+            {
+
+                result = x - y;
+                Calculate.Text = result.ToString();
+            }
+            else if (sum.BackColor == Color.Gray)
+            {
+                result = x + y;
+                Calculate.Text = result.ToString();
+            }
+            else
+            {
+                result = 0;
+                Calculate.Text = "Choose a method!!";
+            }
+        }
+
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            sum.BackColor = DefaultBackColor;
+            minus.BackColor = DefaultBackColor;
+            multiply.BackColor = DefaultBackColor;
+            divide.BackColor = DefaultBackColor;
+            X.Text = "0";
+            Y.Text = "0";
+            Calculate.Text = "0";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            X.Text = "0";
+            Y.Text = "0";
+            Calculate.Text = "0";
+        }
+    }
+}
